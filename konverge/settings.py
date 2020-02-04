62491dc320f7e76a204f6f510cf35a52ed977dc7
DreamPathsProjekt <dream.paths.projekt@gmail.com>
2020-02-04 21:47:24 +0200

    Instantiate both vm and lxc api clients in settings.

--- konverge/settings.py
+++ konverge/settings.py
@@ -4,8 +4,16 @@ from konverge.pve import ProxmoxAPIClient
 # from konverge.utils import FabricWrapper
 
 
-VMFactory = ProxmoxAPIClient.api_client_factory(instance_type='vm')
-vm_client = VMFactory(
+VMAPIClientFactory = ProxmoxAPIClient.api_client_factory(instance_type='vm')
+vm_client = VMAPIClientFactory(
+    host=os.getenv('PROXMOX_HOST'),
+    user=os.getenv('PROXMOX_USER'),
+    password=os.getenv('PROXMOX_PASSWORD')
+)
+
+
+LXCAPIClientFactory = ProxmoxAPIClient.api_client_factory(instance_type='lxc')
+lxc_client = LXCAPIClientFactory(
     host=os.getenv('PROXMOX_HOST'),
     user=os.getenv('PROXMOX_USER'),
     password=os.getenv('PROXMOX_PASSWORD')
