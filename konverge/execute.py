ee3a0d07f6bc7cce87e9cc3471534190892ea614
DreamPathsProjekt <dream.paths.projekt@gmail.com>
2020-02-05 13:49:10 +0200

    Add files module to handle serialization of config files and schema validation.

--- konverge/execute.py
+++ konverge/execute.py
@@ -1,6 +1,7 @@
 from konverge.settings import vm_client
 from konverge.utils import VMAttributes, Storage, FabricWrapper
 from konverge.cloudinit import CloudinitTemplate
+from konverge.files import ProxmoxClusterConfigFile
 
 
 def execute():
@@ -18,13 +19,13 @@ def execute():
         ssh_keyname='vhost3-vms',
         gateway='10.0.100.105'
     )
-    ubuntu_template_factory = CloudinitTemplate.os_type_factory(template_attributes.os_type)
-    ubuntu_template = ubuntu_template_factory(vm_attributes=template_attributes, client=vm_client, proxmox_node=proxmox_node)
-    # print(ubuntu_template.vm_attributes.description)
-    # print(ubuntu_template.download_cloudinit_image())
-    print(ubuntu_template.get_storage(unused=False))
-    ubuntu_template.get_allocated_ips_per_node_interface()
-    # print(client.get_storage_content_items('vhost', type='nfs'))
-    # print(client.get_cluster_node_interfaces())
-    # print(client.get_cluster_node_dns('vhost2'))
+    # ubuntu_template_factory = CloudinitTemplate.os_type_factory(template_attributes.os_type)
+    # ubuntu_template = ubuntu_template_factory(vm_attributes=template_attributes, client=vm_client, proxmox_node=proxmox_node)
+    #
+    # print(ubuntu_template.get_storage(unused=False))
+    # ubuntu_template.get_allocated_ips_per_node_interface()
+
+
+    config = ProxmoxClusterConfigFile().serialize()
+    print(config.nodes[0])
 
